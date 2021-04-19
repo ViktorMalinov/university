bc2e8366a23fa1bd2cc5264b0c8e1d7aa6e6ef42
Viktor Malinov <v.malinov@kodar.net>
2021-04-19 18:01:58 +0300

    final  19.04.2021

--- src/main/common/Utils.java
+++ src/main/common/Utils.java
@@ -1,25 +1,49 @@
 package main.common;
 
+import java.util.Collections;
 import java.util.Set;
 
 public class Utils {
 	
-	public static Long getNextId(Set<Long> set) {
+/*	public static Long getNextId(Set<Long> set) {
 		Long id = 1L;
-		
+
 		if (set.isEmpty()) {
 			return id; 
 		}
-			
+		
+		id = Collections.max(set);
+		
+		return id;
+	}
+*/
+
+	public static Long getNextId(Set<Long> set) {
+		Long id = 0L;
+
+		
+		id = set.stream().mapToLong(v -> v).max().orElse(0L);
+		
+		/*
+		if (set.isEmpty()) {
+			return id; 
+		}
+		
 		for (Long i : set) {
 			if (i > id) {
 				id = i;
 			}
 		}
+		*/
+		
 		
 		id += 1;
+		
 		
 		return id;
 	}
 
+	
+	
+	
 }
